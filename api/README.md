a209b23a8d238f88f6b2383cf3868b5192fa068e
yangqiang <319701058@qq.com>
2022-03-05 20:40:28 +0800

    logistics

---
+++ api/README.md
@@ -0,0 +1,13 @@
+##《叶流》物流管理系统
+
+### 作者-东南大学 srtp小分队
+
+#### 主要框架
+- SpringBoot
+
+- SpringData
+
+- SpringSecurity
+
+- Vue2
+
---
+++ web-app/src/layout/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center', letterSpacing: '1px' }">
+    叶流 ©2021 Created by 东南大学srtp小分队
+  </a-layout-footer>
+</template>
+
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
